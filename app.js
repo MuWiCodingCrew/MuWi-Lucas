e63e17e14679ea6063f36da43f88864ec5fb8bb4
Lucas <Lucas@10.20.141.237>
2018-02-20 13:45:49 +0100

    dummy finished

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const fs = require('fs');
 const mysql = require('mysql');
 const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = express();
+app.use(express.static(__dirname));
 
 // create db connection
 const db = mysql.createConnection({
 	host	:	'localhost',
 	user	:	'admin',
 	password:	'test',
-	database:	'muwi'
+	database:	'test'
 });
 
 // connect to db
@@ -28,99 +28,19 @@ db.connect((err) => {
 	console.log('MySQL connected...');
 });
 
-const app = express();
-
-// SQL creaste db
-/*
-app.get('/createdb', (req, res) => {
-	let sql = 'CREATE DATABASE hatgeklappt';
-	db.query(sql, (err, result) => {
-		if(err){
-			throw err;
-		}
-		console.log(result);
-		res.send('database created...');
-	});
-});
-*/
-
-// SQL create table
-/*
-app.get('/createtable', (req, res) => {
-	let sql = 'CREATE TABLE neu(id AUTO_INCREMENT, title VARCHAR(255), body VARCHAR(255), PRIMARY KEY(id))';
-	db.query(sql, (err, result) => {
-		if(err){
-			throw err;
-		}
-		console.log(result);
-		res.send('table created...');
-	});
-});
-*/
-
-// SQL inserts
-/*
-app.get('/', (req, res) => {
-	let data = {Name:'Rosinski', Vorname:'Ricardo'};
-	let sql = 'INSERT INTO test SET ?';
-	let query = db.query(sql, data, (err, result) => {
-		if(err){
-			throw err;
-		}
-		console.log(result);
-		res.send('line inserted...');
-	});
-});
-*/
-
-// SQL select *
-app.get('/', (req, res) => {
-	let sql = 'SELECT * FROM test';
-	let query = db.query(sql, (err, results) => {
-		if(err){
-			throw err;
-		}
-		console.log(results);
-		res.send('data fetched...');
-	});
-});
-
-//SQL select specific line
-app.get('/:id', (req, res) => {
-	let sql = `SELECT * FROM test WHERE id = ${req.params.id}`;
+app.get('/ajaxcall', function(req, res){
+	var data = {};
+	let sql = `SELECT name, vorname FROM test WHERE ID = '${req.query.id}'`;
 	let query = db.query(sql, (err, result) => {
 		if(err){
 			throw err;
 		}
-		console.log(result);
-		res.send('data fetched...');
+		res.send(result);
 	});
+
 });
 
-app.listen('3000', () => {
+app.listen('3000', (err) => {
 	console.log('Server started on port 3000');
-})
-
-
-
-// start server
-/*
-fs.readFile('index.html', (err, html) => {
-	if(err){
-		throw err;
-	}
-	
-	const server = http.createServer((req, res) => {
-		res.statusCode = 200;
-		res.setHeader('Content-type', 'text/html');
-		res.write(html);
-		res.end();
-	});
-	
-	server.listen(port, hostname, () => {
-		console.log('Server started on port '+port);
-	});
 });
-*/
-
 
